93b27355567676f406a43918c661cbaf1c55014f
Mariam Denyse <mdenyse@er-d5.kzn.21-school.ru>
2021-07-10 20:23:02 +0300

    add_clear_path_of_OLDPWD

--- src/ft_environments.c
+++ src/ft_environments.c
@@ -1,5 +1,19 @@
 #include "../includes/minishell.h"
 
+t_env	*ft_get_list_env(char *find, t_env **env)
+{
+	t_env	*tmp;
+
+	tmp = *env;
+	while (tmp)
+	{
+		if (!ft_strcmp(find, tmp->key))
+			return (tmp);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
+
 int	ft_get_list_environments(char **e, t_env **env)
 {
 	int		i;
@@ -16,5 +30,22 @@ int	ft_get_list_environments(char **e, t_env **env)
 		}
 		ft_add_env(env, new);
 	}
+
+	new = ft_get_list_env("OLDPWD", env);
+	if (new)
+		new->value = NULL;
+	new = *env;
+	while (new)
+	{
+		printf("%s", new->key);
+		if (new->value)
+			printf("=%s\n",new->value);
+		else
+			printf("\n");
+		new = new->next;
+	}
+
+
 	return (1);
+
 }
